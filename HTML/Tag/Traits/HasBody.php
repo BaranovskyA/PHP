69b17c6b36b8c6301808b593ab8ce23192032011
BaranovskyA <54361221+BaranovskyA@users.noreply.github.com>
2020-05-17 14:26:26 +0600

    Add files via upload

---
+++ HTML/Tag/Traits/HasBody.php
@@ -0,0 +1,31 @@
+<?php 
+namespace HTML\Tag\Traits;
+
+use HTML\Tag;
+use HTML\Tag\TagBody;
+
+trait HasBody {
+    protected $body;
+
+    protected function bodyInit() {
+        $this->setBody(new TagBody());
+    }
+
+    function setBody(TagBody $body) {
+        $this->body = $body;
+    }
+
+    function getBody(): TagBody {
+        return $this->body;
+    }
+
+    function appendBody($value) {
+        $this->getBody()->append($value);
+        return $this;
+    }
+
+    function prependBody($value) {
+        $this->getBody()->prepend($value);
+        return $this;
+    }
+}
